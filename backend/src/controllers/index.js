dc4987bf45a4b9dbd154404b0acae619badf24ed
Thiago Marciel <thiagomarciel.dev@gmail.com>
2022-03-01 19:05:39 -0300

    added export Controllers

---
+++ backend/src/controllers/index.js
@@ -0,0 +1,9 @@
+import DevController from "./DevController"
+import LikeController from "./LikeController"
+import DislikeController from "./DislikeController"
+
+export {
+  DevController, 
+  LikeController,
+  DislikeController
+}
