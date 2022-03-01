13adaa75b394d52dbb7337814a03308db4a64aa5
Thiago Marciel <thiagomarciel.dev@gmail.com>
2022-03-01 19:06:12 -0300

    added cors config

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,15 @@
-const express = require('express')
-const mongoose = require('mongoose')
-require('dotenv').config()
+import express from 'express'
+import mongoose from 'mongoose'
+import routes from './routes'
+import cors from 'cors'
 
-const routes = require('./routes')
+require('dotenv').config()
 
 const server = express()
 
 mongoose.connect(`mongodb+srv://${process.env.mongodb_NAME}:${process.env.mongodb_PASSWORD}@cluster0.1aowj.mongodb.net/${process.env.mongodb_PROJECT}?retryWrites=true&w=majority`, {useNewUrlParser:true})
+
+server.use(cors())
 server.use(express.json())
 server.use(routes)
 
