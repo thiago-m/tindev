c35bc2f1296cf8e0bd9e753d867e7021a7158443
Thiago Marciel <thiagomarciel.dev@gmail.com>
2022-03-01 19:05:23 -0300

    added dislikeController

---
+++ backend/src/controllers/DislikeController.js
@@ -0,0 +1,20 @@
+import Dev from '../models/Dev'
+
+export default {
+  async store(req, res) {
+    const { 
+      params: {devId}, 
+      headers: {user} 
+    } = req
+
+    const loggedDev = await Dev.findById(user)
+    const targetDev = await Dev.findById(devId)
+    
+    if(!targetDev) return res.status(400).json({error: 'Dev not exists'})
+    
+    loggedDev.dislikes.push(targetDev._id)
+    await loggedDev.save()
+    
+    return res.json({ loggedDev })
+  }
+}
