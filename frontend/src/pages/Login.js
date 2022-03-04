aba3c571ff1c365e0d73124e2c97a5f28c2700dc
Thiago Marciel <thiagomarciel.dev@gmail.com>
2022-03-03 21:21:43 -0300

    created the functionality to create user on login

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,15 +1,33 @@
-import logo from '../assets/logo.svg'
+import { useNavigate } from "react-router-dom"
+import { useState } from 'react'
 import './Login.css'
 
-export default function Login() {
+import api from '../services/api'
+
+import logo from '../assets/logo.svg'
+
+export default function Login(props) {
+  const [username, setUsername] = useState('')
+  let navigate = useNavigate()
+
+  async function handleSubmit(e) {
+    e.preventDefault()
+
+    const { data: {_id} } = await api.post('/devs', {username})
+
+    navigate(`/dev/${_id}`)
+  }
+
   return (
     <div className='login-container'>
-      <form>
+      <form onSubmit={handleSubmit}>
         <img src={logo} alt="Tinder" />
         <input 
           placeholder='Digite seu usuário do GitHub'
+          value={username}
+          onChange={e => setUsername(e.target.value)}
         />
-        <button>Enviar</button>
+        <button type='submit'>Enviar</button>
       </form>
     </div>
   )
